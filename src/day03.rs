08a07aa3907108ce67aa92697d333577d3c5f105
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-12-03 09:31:36 +0100

    day 3 - part 1

--- src/day03.rs
+++ src/day03.rs
@@ -1,15 +1,38 @@
+use std::collections::HashSet;
+
 type Output = u64;
 
 pub fn part_1(input: &str) -> Output {
-    0
+    input
+        .lines()
+        .map(|line| {
+            let (comp1, comp2) = line.split_at(line.len() / 2);
+            let item = find_misplaced_item(comp1, comp2);
+            score_of(item)
+        })
+        .sum()
 }
 
 pub fn part_2(input: &str) -> Output {
     input.parse().unwrap()
 }
 
-fn find_misplaced_item(rucksack: &str) -> char {
-    'p'
+fn find_misplaced_item(compartment1: &str, compartment2: &str) -> char {
+    let compartment1: HashSet<char> = compartment1.chars().collect();
+    compartment2
+        .chars()
+        .find(|c| compartment1.contains(c))
+        .unwrap_or_default()
+}
+
+fn score_of(item: char) -> Output {
+    if ('a'..='z').contains(&item) {
+        item as Output - 'a' as Output + 1
+    } else if ('A'..='Z').contains(&item) {
+        item as Output - 'A' as Output + 27
+    } else {
+        0
+    }
 }
 
 #[cfg(test)]
@@ -29,12 +52,10 @@ CrZsJsPPZsGzwwsLwLmpwMDw
 
     #[rstest]
     #[case("", 0)]
-    #[ignore = "not implemented"]
     #[case("vJrwpWtwJgWrhcsFMMfFFhFp", 16)]
-    #[ignore = "not implemented"]
+    #[case("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", 38)]
     #[case::example(EXAMPLE, 157)]
-    #[ignore = "not implemented"]
-    #[case::input(INPUT, 0)]
+    #[case::input(INPUT, 7742)]
     fn test_part_1(#[case] input: &str, #[case] expected: Output) {
         assert_eq!(part_1(input.trim()), expected);
     }
@@ -49,8 +70,23 @@ CrZsJsPPZsGzwwsLwLmpwMDw
     }
 
     #[rstest]
-    #[case("vJrwpWtwJgWrhcsFMMfFFhFp", 'p')]
-    fn should_find_misplaced_item(#[case] rucksack: &str, #[case] expected: char) {
-        assert_eq!(find_misplaced_item(rucksack), expected);
+    #[case("ab", "bc", 'b')]
+    #[case("vJrwpWtwJgWr", "hcsFMMfFFhFp", 'p')]
+    fn should_find_misplaced_item(
+        #[case] compartment1: &str,
+        #[case] compartment2: &str,
+        #[case] expected: char,
+    ) {
+        assert_eq!(find_misplaced_item(compartment1, compartment2), expected);
+    }
+
+    #[rstest]
+    #[case('a', 1)]
+    #[case('b', 2)]
+    #[case('z', 26)]
+    #[case('A', 27)]
+    #[case('B', 28)]
+    fn should_find_score(#[case] item: char, #[case] expected: Output) {
+        assert_eq!(score_of(item), expected);
     }
 }
