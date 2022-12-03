f803096a0e6faed4447293deb498d5faa85dbb38
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-12-03 08:59:09 +0100

    start day 3

--- src/day03.rs
+++ src/day03.rs
@@ -1,11 +1,15 @@
 type Output = u64;
 
 pub fn part_1(input: &str) -> Output {
-    input.parse().unwrap()
+    0
 }
 
 pub fn part_2(input: &str) -> Output {
     input.parse().unwrap()
+}
+
+fn find_misplaced_item(rucksack: &str) -> char {
+    'p'
 }
 
 #[cfg(test)]
@@ -13,14 +17,22 @@ mod tests {
     use super::*;
 
     const EXAMPLE: &str = r#"
-
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
     "#;
 
     const INPUT: &str = include_str!("day03/input.txt");
 
     #[rstest]
+    #[case("", 0)]
     #[ignore = "not implemented"]
-    #[case::example(EXAMPLE, 0)]
+    #[case("vJrwpWtwJgWrhcsFMMfFFhFp", 16)]
+    #[ignore = "not implemented"]
+    #[case::example(EXAMPLE, 157)]
     #[ignore = "not implemented"]
     #[case::input(INPUT, 0)]
     fn test_part_1(#[case] input: &str, #[case] expected: Output) {
@@ -34,5 +46,11 @@ mod tests {
     #[case::input(INPUT, 0)]
     fn test_part_2(#[case] input: &str, #[case] expected: Output) {
         assert_eq!(part_2(input.trim()), expected);
+    }
+
+    #[rstest]
+    #[case("vJrwpWtwJgWrhcsFMMfFFhFp", 'p')]
+    fn should_find_misplaced_item(#[case] rucksack: &str, #[case] expected: char) {
+        assert_eq!(find_misplaced_item(rucksack), expected);
     }
 }
