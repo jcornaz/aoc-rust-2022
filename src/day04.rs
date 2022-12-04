fa7108f21efb9d7a3e620fe43e4ddc39e6bf96ec
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-12-04 09:30:07 +0100

    day 4 - part 2

--- src/day04.rs
+++ src/day04.rs
@@ -7,7 +7,14 @@ pub fn part_1(input: &str) -> Output {
 }
 
 pub fn part_2(input: &str) -> Output {
-    input.parse().unwrap()
+    input.lines().filter(|s| is_overlap(s)).count()
+}
+
+fn parse_line(input: &str) -> Option<(RangeInclusive<u32>, RangeInclusive<u32>)> {
+    let (left, right) = input.split_once(',')?;
+    let left = parse_range(left)?;
+    let right = parse_range(right)?;
+    Some((left, right))
 }
 
 fn parse_range(input: &str) -> Option<RangeInclusive<u32>> {
@@ -15,10 +22,13 @@ fn parse_range(input: &str) -> Option<RangeInclusive<u32>> {
     Some(min.parse::<u32>().ok()?..=max.parse::<u32>().ok()?)
 }
 
+fn is_overlap(input: &str) -> bool {
+    let Some((left, right)) = parse_line(input) else { return false };
+    left.contains(right.start()) || left.contains(right.end()) || right.contains(left.start())
+}
+
 fn is_full_overlap(input: &str) -> bool {
-    let Some((left, right)) = input.split_once(',') else { return false };
-    let Some(left) = parse_range(left) else { return false };
-    let Some(right) = parse_range(right) else { return false };
+    let Some((left, right)) = parse_line(input) else { return false };
     (left.contains(right.start()) && left.contains(right.end()))
         || (right.contains(left.start()) && right.contains(left.end()))
 }
@@ -50,10 +60,8 @@ mod tests {
     }
 
     #[rstest]
-    #[ignore = "not implemented"]
-    #[case::example(EXAMPLE, 0)]
-    #[ignore = "not implemented"]
-    #[case::input(INPUT, 0)]
+    #[case::example(EXAMPLE, 4)]
+    #[case::input(INPUT, 895)]
     fn test_part_2(#[case] input: &str, #[case] expected: Output) {
         assert_eq!(part_2(input.trim()), expected);
     }
@@ -73,5 +81,17 @@ mod tests {
     #[case("2-6,4-8", false)]
     fn should_reckognize_full_overlap(#[case] input: &str, #[case] expected: bool) {
         assert_eq!(is_full_overlap(input), expected);
+    }
+
+    #[rstest]
+    #[case("2-4,6-8", false)]
+    #[case("2-3,4-5", false)]
+    #[case("5-7,7-9", true)]
+    #[case("2-8,3-7", true)]
+    #[case("6-6,4-6", true)]
+    #[case("2-6,4-8", true)]
+    #[case("3-4,1-5", true)]
+    fn should_reckognize_partial_overlap(#[case] input: &str, #[case] expected: bool) {
+        assert_eq!(is_overlap(input), expected);
     }
 }
